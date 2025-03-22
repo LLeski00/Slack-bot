62fd9790d04bf6da4d0f07573bc1fe94be8a1b1d
Luka <llesko00@fesb.hr>
2025-03-22 14:38:01 +0100

    Expand usage message

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const MIN_SCORE = 70;
 const MAX_ITEMS = 3;
-const USAGE_MESSAGE = "Korištenje: @LocaBot <predmet_koji_tražite>";
+const USAGE_MESSAGE =
+    "Korištenje:\n@LocaBot <predmet_koji_tražite> - Pretražuje predmete i vraća njihovu lokaciju\n@LocaBot refresh - Osvježava podatke ponovnom pretragom informacija o lokaciji predmeta";
 const REFRESH_MESSAGE = "Podaci su uspješno osvježeni.";
 
 module.exports = { MIN_SCORE, MAX_ITEMS, USAGE_MESSAGE, REFRESH_MESSAGE };
